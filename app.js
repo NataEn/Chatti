73bde066701edd71bdcd11dfe0c9861fac155dca
natalie <natalie@enkin.co.il>
2020-03-17 15:02:10 +0200

    establishing socket connection between the browser and the server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
+
 const server = app.listen(3000, () => {
   console.log("listening to requests on port 3000...");
 });
 
 app.use(express.static("static"));
+
+const io = socket(server);
+io.on("connection", socket => {
+  console.log("connectting with socket...", socket.id);
+});
