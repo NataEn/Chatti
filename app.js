234474c55a5937bb626932042eb1005b8174b327
natalie <natalie@enkin.co.il>
2020-03-19 15:48:43 +0200

    sending and recieving image files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const socket = require("socket.io");
+const socketStream = require("socket.io-stream");
 const fs = require("fs");
 const path = require("path");
 const app = express();
@@ -22,21 +23,43 @@ app.use(express.static("static"));
 const io = socket(server);
 io.on("connection", socket => {
   console.log("connectting user with socket...", socket.id);
+
+  //send stream to client/browser
+
+  // console.log("file being send");
+  // let stream = socketStream.createStream();
+  // socketStream(socket).emit("getFile", stream);
+  // let filename = "./static/userFiles/cat.jpeg";
+  // console.log(filename);
+  // fs.createReadStream(filename).pipe(stream);
+
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
   });
   socket.on("newMessage", data => {
     console.log("got data from user");
-    if (data.files) {
+    if (data.files && data.files.length) {
       for (let i = 0; i < data.files.length; i++) {
-        rawData = data.files[i].content;
-        base64Data = rawData.replace(/^data:image\/(.*);base64,/, "");
+        let rawData = data.files[i].content;
+        let base64Data = rawData.replace(/^data:image\/(.*);base64,/, "");
+
         // dataimage = new Buffer(base64Data, "base64"); not needed
+
         const fd = path.join("./static/userFiles", data.files[i].name);
         ensureDirectoryExistence(fd);
+
+        //saving data recieved from client to server on file:
         fs.writeFile(fd, base64Data, "base64", err => {
           console.log(err);
         });
+
+        // //emitting the data back to the chat window:
+        // const stream = socketStream.createStream();
+        // socketStream(socket).emit("getFile", stream, {
+        //   filename: data.files[i].name
+        // });
+        // console.log(filename + " is streaming...");
+        // fs.createReadStream(fd).pipe(stream);
       }
     }
     io.sockets.emit("newMessage", data);
--- static/chat.js
+++ static/chat.js
@@ -1,5 +1,6 @@
 const socket = io.connect("http://localhost:3000");
 //dom elements
+
 const message = document.querySelector("#message");
 const name = document.querySelector("#name");
 const submitBtn = document.querySelector("#submit");
@@ -10,15 +11,27 @@ const inputPicture = document.querySelector("input.picture");
 const file = document.querySelector("#file");
 const inputFile = document.querySelector("input.file");
 const picAmount = document.querySelector("#picAmount");
+
 //data
 const userData = { name: "", text: "", files: [] };
-
 socket.on("connect", () => {
   console.log("connected to server");
+  // ///from example:
+
+  // $("input.picture").change(function(e) {
+  //   let file = e.target.files[0];
+  //   let stream = ss.createStream();
+  //   // upload a file to the server.
+  //   ss(socket).emit("send-file", stream, file);
+  //   ss.createBlobReadStream(file).pipe(stream);
+  //   $("input.picture").val("");
+  //   $("input.picture").after("<p>File uploaded!</p>");
+  // });
 });
 socket.on("disconnect", () => {
   console.log("disconnected from server");
 });
+
 socket.on("newMessage", data => {
   const incomingMessage = document.createElement("li");
   incomingMessage.classList = "bg-light list-group-item";
@@ -26,14 +39,36 @@ socket.on("newMessage", data => {
   const sender = document.createElement("span");
   sender.className = "text-primary";
   sender.innerText = `${data.name}: `;
+
   const content = document.createElement("span");
   content.innerText = data.text;
+
   incomingMessage.appendChild(sender);
   incomingMessage.appendChild(content);
   chat.appendChild(incomingMessage);
+  if (data.files.length) {
+    const img = document.createElement("img");
+    // img.src = "data:image/jpeg;base64," + window.btoa(data.files[0].content);
+    img.src = data.files[0].content;
+    chat.appendChild(img);
+  }
 });
 socket.on("typing", data => {
   typing.innerText = `${data.name} is typing...`;
+});
+
+socket.on("getFile", (stream, data) => {
+  console.log("start file stream from server", data);
+  const imgChunks = "";
+  stream.on("data", data => {
+    console.log("got data from getFile");
+    for (let i = 0; i < data.length; i++) {
+      imgChunks += String.fromCharCode(data[i]);
+    }
+  });
+  stream.on("end", data => {
+    console.log("end of stream");
+  });
 });
 
 name.oninput = e => {
@@ -50,12 +85,6 @@ submitBtn.addEventListener("click", e => {
   socket.emit("newMessage", userData);
 });
 inputPicture.onchange = e => {
-  console.log("entered input file");
-  console.log(
-    inputPicture.value,
-    inputPicture.files,
-    inputPicture.files.length
-  );
   picAmount.classList.toggle("d-none");
   picAmount.innerText = inputPicture.files.length;
   for (let i = 0; i < inputPicture.files.length; i++) {
@@ -76,7 +105,6 @@ inputPicture.onchange = e => {
     };
     reader.readAsDataURL(inputPicture.files[i]);
   }
-  console.log(userData);
 };
 
 picture.onclick = e => {
