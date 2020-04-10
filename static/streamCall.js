d8bf87979d8420e94582ba12e034abe14fcf5803
natalie <natalie@enkin.co.il>
2020-04-10 20:51:46 +0300

    changing call to media devices

--- static/streamCall.js
+++ static/streamCall.js
@@ -2,17 +2,72 @@ const remoteVideo = document.querySelector("#remoteVideo");
 const localVideo = document.querySelector("#localVideo");
 const usersId = {
   initiator: "",
-  contacts: []
+  contacts: [],
 };
+const desktopConstarains = {
+  video: {
+    manadatory: { maxWidth: 800, maxHeight: 600 },
+  },
+  audio: true,
+};
+const mobileConstarains = {
+  video: {
+    manadatory: { maxWidth: 480, maxHeight: 320 },
+  },
+  audio: true,
+};
+const setConstrains = () => {
+  let constrains = {};
+  if (/Android|iPhone|ipad/i.test(navigator.userAgent)) {
+    constrains = mobileConstarains;
+  } else {
+    constrains = desktopConstarains;
+  }
+  return constrains;
+};
+const hasUserMedia = () => {
+  //check if the browser supports the WebRTC
+  return !!(
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia
+  );
+};
+if (hasUserMedia()) {
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+  //enabling video and audio channels
+  navigator.getUserMedia(
+    constraints,
+    function (stream) {
+      var video = document.querySelector("video");
+
+      //inserting our stream to the video tag
+      video.src = window.URL.createObjectURL(stream);
+    },
+    function (err) {}
+  );
+} else {
+  alert("WebRTC is not supported");
+}
 
 const startVideoCall = () => {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(stream => {
+  let constrains = setConstrains();
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+  navigator
+    .getUserMedia(constrains)
+    .then((stream) => {
       const peer = new SimplePeer({
         initiator: location.hash === "#init",
         trickle: false,
-        stream: stream
+        stream: stream,
       });
       otherPeer = new SimplePeer();
       if (peer.initiator) {
@@ -20,29 +75,29 @@ const startVideoCall = () => {
         // peer.signal(recordsState.filmCall.streamObj);
       }
 
-      peer.on("signal", function(data) {
+      peer.on("signal", function (data) {
         usersId.initiator = data;
         console.log("initiator", data);
         document.getElementById("yourId").value = JSON.stringify(data);
       });
 
-      document.getElementById("connect").addEventListener("click", function() {
+      document.getElementById("connect").addEventListener("click", function () {
         let otherId = JSON.parse(document.getElementById("otherId").value);
         peer.signal(otherId);
       });
 
       document
         .getElementById("yourMessage")
-        .addEventListener("change", function() {
+        .addEventListener("change", function () {
           let yourMessage = document.getElementById("yourMessage").value;
           peer.send(yourMessage);
         });
 
-      peer.on("data", function(data) {
+      peer.on("data", function (data) {
         document.getElementById("messages").textContent += data + "\n";
       });
       //open with other device and see if got stream correctly
-      peer.on("stream", stream => {
+      peer.on("stream", (stream) => {
         if (!peer.initiator) {
           remoteVideo.srcObject = stream;
           remoteVideo.play();
@@ -53,7 +108,7 @@ const startVideoCall = () => {
         console.log("got stream:", stream);
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
