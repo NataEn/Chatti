8978af975d0e479abd7551d922403da73247d07e
natalie <natalie@enkin.co.il>
2020-04-10 21:53:14 +0300

    stream with webRtc without p2p library

--- static/streamCall.js
+++ static/streamCall.js
@@ -47,49 +47,53 @@ const startVideoCall = () => {
   navigator.getUserMedia(
     constrains,
     function (stream) {
-      const peer = new SimplePeer({
-        initiator: location.hash === "#init",
-        trickle: false,
-        stream: stream,
-      });
-      otherPeer = new SimplePeer();
-      if (peer.initiator) {
-        console.log("stream to be sent", stream);
-        // peer.signal(recordsState.filmCall.streamObj);
-      }
+      const video = document.querySelector(".video");
 
-      peer.on("signal", function (data) {
-        usersId.initiator = data;
-        console.log("initiator", data);
-        document.getElementById("yourId").value = JSON.stringify(data);
-      });
+      //inserting our stream to the video tag
+      video.src = window.URL.createObjectURL(stream);
+      // const peer = new SimplePeer({
+      //   initiator: location.hash === "#init",
+      //   trickle: false,
+      //   stream: stream,
+      // });
+      // otherPeer = new SimplePeer();
+      // if (peer.initiator) {
+      //   console.log("stream to be sent", stream);
+      //   // peer.signal(recordsState.filmCall.streamObj);
+      // }
 
-      document.getElementById("connect").addEventListener("click", function () {
-        let otherId = JSON.parse(document.getElementById("otherId").value);
-        peer.signal(otherId);
-      });
+      // peer.on("signal", function (data) {
+      //   usersId.initiator = data;
+      //   console.log("initiator", data);
+      //   document.getElementById("yourId").value = JSON.stringify(data);
+      // });
 
-      document
-        .getElementById("yourMessage")
-        .addEventListener("change", function () {
-          let yourMessage = document.getElementById("yourMessage").value;
-          peer.send(yourMessage);
-        });
+      // document.getElementById("connect").addEventListener("click", function () {
+      //   let otherId = JSON.parse(document.getElementById("otherId").value);
+      //   peer.signal(otherId);
+      // });
 
-      peer.on("data", function (data) {
-        document.getElementById("messages").textContent += data + "\n";
-      });
-      //open with other device and see if got stream correctly
-      peer.on("stream", (stream) => {
-        if (!peer.initiator) {
-          remoteVideo.srcObject = stream;
-          remoteVideo.play();
-        } else if (peer.initiator) {
-          localVideo.srcObject = stream;
-          localVideo.play();
-        }
-        console.log("got stream:", stream);
-      });
+      // document
+      //   .getElementById("yourMessage")
+      //   .addEventListener("change", function () {
+      //     let yourMessage = document.getElementById("yourMessage").value;
+      //     peer.send(yourMessage);
+      //   });
+
+      // peer.on("data", function (data) {
+      //   document.getElementById("messages").textContent += data + "\n";
+      // });
+      ////open with other device and see if got stream correctly
+      // peer.on("stream", (stream) => {
+      //   if (!peer.initiator) {
+      //     remoteVideo.srcObject = stream;
+      //     remoteVideo.play();
+      //   } else if (peer.initiator) {
+      //     localVideo.srcObject = stream;
+      //     localVideo.play();
+      //   }
+      //   console.log("got stream:", stream);
+      // });
     },
     function (err) {
       console.log(err);
